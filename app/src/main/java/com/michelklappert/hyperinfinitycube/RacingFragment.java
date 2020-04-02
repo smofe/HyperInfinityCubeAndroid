969081752f7319ff7c691a8be0dd794fc91bf403
smofe <44811445+smofe@users.noreply.github.com>
2020-04-02 16:48:45 +0200

    Started Racing Mode Implementation

--- app/src/main/java/com/michelklappert/hyperinfinitycube/RacingFragment.java
+++ app/src/main/java/com/michelklappert/hyperinfinitycube/RacingFragment.java
@@ -6,14 +6,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class RacingFragment extends Fragment {
+
+    private ColorpickerFragment colorPicker;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_racing, null);
+
+        /* Load the colorPicker fragment */
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        colorPicker = new ColorpickerFragment();
+        colorPicker.setArguments(getActivity().getIntent().getExtras());
+        transaction.replace(R.id.racing_colorPickerContainer,colorPicker).commit();
 
         return root;
     }
