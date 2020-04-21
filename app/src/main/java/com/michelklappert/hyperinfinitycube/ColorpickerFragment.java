8988725386cb6d02adc5bbc47434d585d6fb708c
smofe <44811445+smofe@users.noreply.github.com>
2020-04-21 17:58:12 +0200

    Changed Color structure

--- app/src/main/java/com/michelklappert/hyperinfinitycube/ColorpickerFragment.java
+++ app/src/main/java/com/michelklappert/hyperinfinitycube/ColorpickerFragment.java
@@ -1,10 +1,14 @@
 package com.michelklappert.hyperinfinitycube;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.database.DatabaseReference;
@@ -35,6 +39,8 @@ public class ColorpickerFragment extends Fragment {
 
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_colorpicker, null);
 
+        observerList = new ArrayList<ColorObserver>();
+
         colorPicker = (ColorPicker) root.findViewById(R.id.color_Colorpicker);
         saturationBar = (SaturationBar) root.findViewById(R.id.saturationbar);
 
@@ -42,9 +48,22 @@ public class ColorpickerFragment extends Fragment {
         colorPicker.setShowOldCenterColor(false);
 
         colorPicker.setOnColorChangedListener(new ColorPicker.OnColorChangedListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onColorChanged(int color) {
-                if (dbColorRef != null) dbColorRef.setValue(color);
+                //int hexColor = Integer.decode(String.format("0x%06X", (0xFFFFFF & color)));
+                int red = Color.red(color);
+                int green = Color.green(color);
+                int blue = Color.blue(color);
+                int alpha = Color.alpha(color);
+                Log.i("color", "r: " + red + " b: " + blue + " g:" + green + " a: " + alpha);
+                if (dbColorRef != null) {
+                    dbColorRef.child("r").setValue(Color.red(color));
+                    dbColorRef.child("g").setValue(Color.green(color));
+                    dbColorRef.child("b").setValue(Color.blue(color));
+                    dbColorRef.child("a").setValue(Color.alpha(color));
+                    //dbColorRef.setValue(hexColor);
+                }
                 for (int i=0; i < observerList.size(); i++){
                     observerList.get(i).update(color);
                 }
